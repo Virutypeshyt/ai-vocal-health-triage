52b30d978b75a7d304a26252f9f8c55212407cb4
Virendra Gaikwad <virendragaikwad.pvr@gmail.com>
2026-03-25 22:52:56 +0530

    Refactor diagnosis logic for cough and hoarseness
    Refactored diagnosis logic to include hoarseness checks.

--- src/Main.java
+++ src/Main.java
@@ -15,16 +15,21 @@ public class Main {
         System.out.print("Do you have fever? (yes/no): ");
         String fever = sc.nextLine().toLowerCase();
 
-        System.out.println("\n--- Diagnosis ---");
+System.out.println("\n--- Diagnosis ---");
 
-        
-        if (cough.equals("yes") && fever.equals("yes")) {
-            System.out.println("Possible Condition: Infection");
-            System.out.println("Reason: cough + fever detected");
-        } else {
-            System.out.println("No major condition detected");
-        }
 
-        sc.close();
-    }
+if (cough.equals("yes") && fever.equals("yes")) {
+    System.out.println("Possible Condition: Infection");
+    System.out.println("Reason: cough + fever detected");
+}
+else if (hoarseness.equals("yes") && cough.equals("yes")) {
+    System.out.println("Possible Condition: Vocal Strain");
+    System.out.println("Reason: hoarseness + cough detected");
+}
+else if (hoarseness.equals("yes")) {
+    System.out.println("Possible Condition: Mild Voice Issue");
+    System.out.println("Reason: hoarseness detected");
+}
+else {
+    System.out.println("No major condition detected");
 }
